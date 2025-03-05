04b0f97d5244e8889af3c447fa2daac7db6ae44c
seohyeong <jseoh95@gmail.com>
2025-03-05 16:06:19 +0900

    display supporting videos

--- app/app.py
+++ app/app.py
@@ -2,6 +2,30 @@
 import streamlit as st
 from api_utils import get_api_response
 
+def format_timestamp(timestamp: str) -> str:
+    ts = timestamp.strip().split(":")
+    if len(ts) == 3:
+        return "{}h{}m{}s".format(ts[0], ts[1], ts[2])
+    elif len(ts) == 2:
+        return "{}m{}s".format(ts[0], ts[1])
+    elif len(ts) == 1:
+        return "{}s".format(ts[0])
+    else:
+        return None
+    
+    
+def convert_to_sec(timestamp: str) -> int:
+    ts = timestamp.strip().split(":")
+    if len(ts) == 3:
+        return int(ts[0]) * 3600 + int(ts[1]) * 60 + int(ts[2])
+    elif len(ts) == 2:
+        return int(ts[0]) * 60 + int(ts[1])
+    elif len(ts) == 1:
+        return int(ts[0])
+    else:
+        return None
+    
+    
 def display_chat_interface():
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -9,7 +33,7 @@ def display_chat_interface():
 
     if prompt := st.chat_input("Query:"):
         st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
+        with st.chat_message("user", avatar="🧐"):
             st.markdown(prompt)
 
         with st.spinner("Generating response..."):
@@ -19,18 +43,41 @@ def display_chat_interface():
                 st.session_state.session_id = response.get('session_id')
                 st.session_state.messages.append({"role": "assistant", "content": response['answer']})
                 
-                with st.chat_message("assistant"):
+                with st.chat_message("assistant", avatar="🤖"):
                     st.markdown(response['answer'])
                     
-                    with st.expander("Details"):
-                        st.subheader("Generated Answer")
-                        st.code(response['answer'])
-                        st.subheader("Session ID")
-                        st.code(response['session_id'])
+                    if response["docs"]:
+                        st.write("Check out more information in following segments:")
+                        num_videos = len(response["docs"])
+                        
+                        for i in range(num_videos):
+                            col_video, col_meta = st.columns([2, 1.5])
+                            
+                            video_title = response["docs"][i]["title"]
+                            video_header = response["docs"][i]["header"]
+                            video_start = response["docs"][i]["time_start"]
+                            video_end = response["docs"][i]["time_end"]
+                            f_video_start, f_video_end = format_timestamp(video_start), format_timestamp(video_end)
+                            video_url = "https://www.youtube.com/{}".format(
+                                response["docs"][i]["video_id"]
+                            )
+                            video_url_start_end = "https://www.youtube.com/{}?start={}&end={}".format(
+                                response["docs"][i]["video_id"],
+                                str(convert_to_sec(video_start)),
+                                str(convert_to_sec(video_end))
+                                )
+                                
+                            with col_video:
+                                st.video(video_url, start_time=f_video_start, end_time=f_video_end)
+                            with col_meta:
+                                st.markdown("""
+                                            **{}**  
+                                            *{}*
+                                            """.format(video_title, video_header))
+                                st.caption(video_url_start_end) 
             else:
                 st.error("Failed to get a response from the API. Please try again.")
-                
-                
+                           
                 
 st.title("Huberman RAG Chatbot")
 
