4da83d418b23c6a372eb2f7f670488dc97bb5bce
seohyeong <jseoh95@gmail.com>
2025-03-06 18:38:42 +0900

    keep track of video meta info

--- app/app.py
+++ app/app.py
@@ -34,34 +34,55 @@ if "chat_history" not in st.session_state:
 
 if "session_id" not in st.session_state:
     st.session_state.session_id = None
+    
+if "videos" not in st.session_state:
+    st.session_state.videos = []
 
 
-# Display the chat interface
-for message in st.session_state.chat_history:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# Display chat and video history
+for messages, videos in zip(st.session_state.chat_history, st.session_state.videos):
+    print(messages)
+    print(messages[0])
+    for message in messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    for video in videos:
+        col_video, col_meta = st.columns([2, 1.5])
+        with col_video:
+            st.video(video["video_url"], start_time=video["f_video_start"], end_time=video["f_video_end"])
+        with col_meta:
+            st.markdown("""
+                        **{}**  
+                        :gray[*{}*]
+                        """.format(video["video_title"], video["video_header"]))
+            st.markdown("[Watch it on Youtube](%s)" % video["video_url_start_end"])
+        
 
 if prompt := st.chat_input("Query:"):
-    st.session_state.chat_history.append({"role": "user", "content": prompt})
     with st.chat_message("user", avatar="🧐"):
         st.markdown(prompt)
 
     with st.spinner("Generating response..."):
+        flattened_chat_history = [item for sublist in st.session_state.chat_history for item in sublist]
+        print(flattened_chat_history)
         response = get_api_response(question = prompt, 
-                                    chat_history = st.session_state.chat_history, 
+                                    chat_history = flattened_chat_history, 
                                     session_id = st.session_state.session_id)
         
         if response:
             st.session_state.session_id = response.get('session_id')
-            st.session_state.chat_history.append({"role": "assistant", "content": response['answer']})
+            st.session_state.chat_history.append([{"role": "user", "content": prompt},
+                                                  {"role": "assistant", "content": response['answer']}])
+            print("chat_history: ", st.session_state.chat_history)
             
             with st.chat_message("assistant", avatar="🤖"):
                 st.markdown(response['answer'])
-                
+
                 if response["docs"]:
                     st.write("Check out more information in following segments:")
                     num_videos = len(response["docs"])
                     
+                    session_video_info = []
                     for i in range(num_videos):
                         col_video, col_meta = st.columns([2, 1.5])
                         
@@ -70,10 +91,10 @@ if prompt := st.chat_input("Query:"):
                         video_start = response["docs"][i]["time_start"]
                         video_end = response["docs"][i]["time_end"]
                         f_video_start, f_video_end = format_timestamp(video_start), format_timestamp(video_end)
-                        video_url = "https://www.youtube.com/{}".format(
+                        video_url = "https://www.youtube.com/watch?v={}".format(
                             response["docs"][i]["video_id"]
                         )
-                        video_url_start_end = "https://www.youtube.com/{}?start={}&end={}".format(
+                        video_url_start_end = "https://www.youtube.com/watch?v={}?start={}&end={}".format(
                             response["docs"][i]["video_id"],
                             str(convert_to_sec(video_start)),
                             str(convert_to_sec(video_end))
@@ -84,8 +105,19 @@ if prompt := st.chat_input("Query:"):
                         with col_meta:
                             st.markdown("""
                                         **{}**  
-                                        *{}*
+                                        :gray[*{}*]
                                         """.format(video_title, video_header))
-                            st.caption(video_url_start_end) 
+                            st.markdown("[Watch it on Youtube](%s)" % video_url_start_end)
+                        
+                        video_info = {
+                            "video_url": video_url,
+                            "f_video_start": f_video_start,
+                            "f_video_end": f_video_end,
+                            "video_title": video_title,
+                            "video_header": video_header,
+                            "video_url_start_end": video_url_start_end
+                        }
+                        session_video_info.append(video_info)     
+                st.session_state.videos.append(session_video_info)    
         else:
             st.error("Failed to get a response from the API. Please try again.")
