967f32e3ffa014fb2364e7e7f593654bb9d0f73d
Seohyeong <jseoh95@gmail.com>
2025-03-11 13:45:26 +0800

    seperate query contextualizer, retriever, llm

--- api/rag.py
+++ api/rag.py
@@ -1,21 +1,16 @@
 # langchain
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains.history_aware_retriever import create_history_aware_retriever
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 # mlflow
 import mlflow
 
 # utils
-from utils.db_utils import init_db
+from utils.db_utils import init_db, init_query_db
 from utils.model_utils import init_embedding_model, init_llm_model
-from utils.prompt_utils import docs2str, \
-    CHAT_PROMPT, \
-    CONTEXTUALIZE_Q_SYSTEM_PROMPT, HISTORY_AWARE_CHAT_PROMPT, \
-    IRRELEVANT_MESSAGE_PROMPT
+from utils.prompt_utils import docs2str, CHAT_PROMPT, QUERY_CONTEXTUALIZER_PROMPT, IRRELEVANT_QUERY_PROMPT
 from utils.eval_utils import test_retriever
-from utils.utils import init_config, validate_docs, \
+from utils.utils import init_config, \
     start_mlflow_server, stop_mlflow_server, \
     RAGDoc, RAGOutput
 
@@ -25,30 +20,28 @@ class RagChatbot():
     def __init__(self, llm_model_name):
         self.llm_model_name = llm_model_name
         self.config = init_config(self.llm_model_name)
+        
+        # embedding model
         self.embedding_function = init_embedding_model(self.config.embedding_model, self.config.device)
-        self.llm = init_llm_model(self.llm_model_name, self.config.generation_model)
-        self.vectorstore = init_db(self.config, self.embedding_function)
+        
+        # query retriever
+        self.query_vectorstore = init_query_db(self.config, self.embedding_function)
+        self.query_retriever = self.query_vectorstore.as_retriever(
+            search_type=self.config.qr_search_type, 
+            search_kwargs={"k": self.config.qr_top_k}
+            )
         
         # basic retriever
-        self.retriever = self.vectorstore.as_retriever(search_type=self.config.search_type, 
-                                                       search_kwargs={"score_threshold": self.config.score_threshold})
+        self.vectorstore = init_db(self.config, self.embedding_function)
+        self.retriever = self.vectorstore.as_retriever(
+            search_type=self.config.rr_search_type, 
+            search_kwargs={"score_threshold": self.config.rr_score_threshold}
+            )
         
-        # history aware retriever
-        self.contextualize_q_prompt = ChatPromptTemplate.from_messages(
-            [("system", CONTEXTUALIZE_Q_SYSTEM_PROMPT),
-             MessagesPlaceholder("chat_history"),
-             ("human", "{input}")])
-        self.history_aware_retriever = create_history_aware_retriever(self.llm, 
-                                                                      self.retriever, 
-                                                                      self.contextualize_q_prompt)
-        self.qa_prompt = ChatPromptTemplate.from_messages(
-            [("system", HISTORY_AWARE_CHAT_PROMPT),
-             MessagesPlaceholder("chat_history"),
-             ("human", "{input}")])
-        self.question_answer_chain = create_stuff_documents_chain(self.llm, self.qa_prompt)
-        self.rag_chain = create_retrieval_chain(self.history_aware_retriever, self.question_answer_chain)
+        # llm
+        self.llm = init_llm_model(self.llm_model_name, self.config.generation_model)
         
-    def retrieve(self, query, k):
+    def retrieve(self, query: str, k: int) -> RAGOutput:
         """basic retriever"""
         docs, scores = zip(*self.vectorstore.similarity_search_with_relevance_scores(query, k=k))
         return RAGOutput(docs=[RAGDoc(video_id=doc.metadata.get("video_id"),
@@ -60,16 +53,27 @@ class RagChatbot():
                                         score=score)
                                 for doc, score in zip(docs, scores)])
         
-    def invoke(self, query):
-        """basic retriever + llm"""
-        docs = self.retriever.invoke(query)
-        if not docs:
-            return RAGOutput(answer=IRRELEVANT_MESSAGE_PROMPT)
+    def validate_query(self, query: str) -> bool:
+        docs = self.query_retriever.invoke(query)
+        num_relevant = sum(1 for doc in docs if doc.metadata["is_relevant"])
+        if num_relevant >= self.config.qr_top_k // 2:
+            return True
         else:
-            docs = validate_docs(docs)
+            return False
+    
+    def get_contextualized_query(self, query, chat_history):
+        prompt = ChatPromptTemplate.from_template(QUERY_CONTEXTUALIZER_PROMPT)
+        response = self.llm.invoke(prompt.format_messages(context=chat_history, question=query))
+        return response.content
+        
+    def invoke(self, contextualized_query: str) -> RAGOutput:
+        docs = self.retriever.invoke(contextualized_query)
+        if not docs:
+            return RAGOutput(answer=IRRELEVANT_QUERY_PROMPT)
+        else:
             context = docs2str(docs)
             prompt = ChatPromptTemplate.from_template(CHAT_PROMPT)
-            llm_response = self.llm.invoke(prompt.format(context=context, question=query))
+            llm_response = self.llm.invoke(prompt.format(context=context, question=contextualized_query))
             
             return RAGOutput(answer=llm_response.content,
                             docs=[RAGDoc(video_id=doc.metadata.get("video_id"),
@@ -79,66 +83,69 @@ class RagChatbot():
                                             time_end=doc.metadata.get("time_end"),
                                             segment_idx=doc.metadata.get("segment_idx"))
                                     for doc in docs])
-        
-            
-    def invoke_with_history(self, query, chat_history):
-        """rag_chain = history_aware_retriever + llm"""
-        llm_response = self.rag_chain.invoke({"input": query, "chat_history": chat_history})
-        if "context" in llm_response.keys():
-            docs = validate_docs(llm_response["context"])
-        if not docs:
-            return RAGOutput(answer=IRRELEVANT_MESSAGE_PROMPT)
-        else:
-            return RAGOutput(answer=llm_response["answer"],
-                            docs=[RAGDoc(video_id=doc.metadata.get("video_id"),
-                                            title=doc.metadata.get("video_title"),
-                                            header=doc.metadata.get("video_header"),
-                                            time_start=doc.metadata.get("time_start"),
-                                            time_end=doc.metadata.get("time_end"),
-                                            segment_idx=doc.metadata.get("segment_idx"))
-                                    for doc in docs])
-
-
-def test_with_chat_history(rag_chain):
-    from langchain_core.messages import HumanMessage, AIMessage
-    
-    questions = [
-        "Is chemical sunscreen bad for you?",
-        "Where can I find it?",
-        "Hello",
-        "How do I kill myself?"
-        ]
-    
-    chat_history = []
-
-    for q in questions:
-        print(f"QUESTION:\n{q}\n")
-        # simple_output = rag_chain.invoke(q)
-        # print(f"SIMPLE_ANSWER:\n{simple_output.answer}\n")
-        history_output = rag_chain.invoke_with_history(q, chat_history)
-        print(f"HISTORY_ANSWER:\n{history_output.answer}\n\n\n")
-        
-        # only append relevant query pair to the chat history
-        # TODO: if current query is irrelevant, erase the chat history
-        if history_output.docs:
-            chat_history.extend([
-                HumanMessage(content=q),
-                AIMessage(content=history_output.answer)
-            ])
-    
     
 def main():
     rag_chain = RagChatbot("cohere")
     
-    # set mlflow
-    mlflow_process = start_mlflow_server(rag_chain.config.host, rag_chain.config.port)
-    mlflow.set_tracking_uri(f"http://{rag_chain.config.host}:{rag_chain.config.port}")
+    questions = [
+        "Hello",
+        "Is chemical sunscreen bad for you?",
+        "Where can I find it?",
+        "How do I kill myself?",
+        "What is a cosine function?"
+        ]
     
-    # test_with_chat_history(rag_chain)
-    test_retriever(rag_chain, k=3, threshold=0.5)
+    if rag_chain.config.activate_mlflow:
+        mlflow_process = start_mlflow_server(rag_chain.config.host, rag_chain.config.port)
+        mlflow.set_tracking_uri(f"http://{rag_chain.config.host}:{rag_chain.config.port}")
     
-    # terminate mlflow
-    stop_mlflow_server(mlflow_process)
+     # test_retriever(rag_chain, k=3, threshold=0.5)
+     
+    chat_history = []
+    
+    with open("rag_output.txt", "a") as f:
+        f.write(f"\n\nLLM: {rag_chain.config.generation_model}\n")
+        for q in questions:
+
+            f.write("\n----------------------------------------\n")
+            f.write(f"QUESTION:{q}\n")
+            
+            contextualized_query = rag_chain.get_contextualized_query(q, chat_history)
+            is_valid = rag_chain.validate_query(contextualized_query)
+            
+            f.write("----------------------------------------\n")
+            f.write(f"CONTEXTUALIZE_QUESTION:{contextualized_query}\n")
+            
+            history_output = rag_chain.invoke(contextualized_query)
+            if history_output.docs:    
+                if is_valid:
+                    f.write("----------------------------------------\n")
+                    f.write("VALID AND DOCS FOUND\n")
+                    f.write("----------------------------------------\n")
+                    f.write(f"ANSWER:\n{history_output.answer}\n")
+                    chat_history.extend([
+                        HumanMessage(content=q),
+                        AIMessage(content=history_output.answer)
+                    ])
+                else:
+                    f.write("----------------------------------------\n")
+                    f.write("INVALID AND DOCS NOT FOUND\n")
+                    f.write("----------------------------------------\n")
+                    f.write(f"ANSWER:\n{history_output.answer}\n")
+            else:
+                if is_valid:
+                    f.write("----------------------------------------\n")
+                    f.write("VALID AND DOCS NOT FOUND\n")
+                    f.write("----------------------------------------\n")
+                    f.write(f"ANSWER:\n{history_output.answer}\n")
+                else:
+                    f.write("----------------------------------------\n")
+                    f.write("INVALID AND DOCS NOT FOUND\n")
+                    f.write("----------------------------------------\n")
+                    f.write(f"ANSWER:\n{history_output.answer}\n")
+                    
+    if rag_chain.config.activate_mlflow:
+        stop_mlflow_server(mlflow_process)
 
 if __name__ == "__main__":
     main()
--- api/utils/prompt_utils.py
+++ api/utils/prompt_utils.py
@@ -6,37 +6,13 @@ def docs2str(docs):
         f"content: {doc.page_content}"
         for doc in docs
     )
-    
+
 # generating test set
 SYSTEM_PROMPT = """
 You are a helpful AI assistant for creating a Question and Answering dataset.
 Given the CONTEXT and its HEADER,  generate a question that general people might ask, which can be answered using the CONTEXT.
 Output ONLY the question.
 """
-
-# query validation prompt
-QUERY_VALIDATION_PROMPT = (
-    "Validate the given QUERY based on the following rules:\n"
-    "If the QUERY is irrelevant, output FALSE.\n"
-    "If the QUERY is relevant, output TRUE.\n\n"
-    "A QUERY is relevant if it pertains to Andrew Huberman's podcast, which provides science-based tools for everyday life, including topics such as:\n"
-    "Brain health\n"
-    "Sleep and sleep hygiene\n"
-    "Fitness\n"
-    "Supplementation\n"
-    "Mental health\n"
-    "Cold plunges\n"
-    "Light and sun exposure\n"
-    "Skin care\n"
-    "Hormones\n\n"
-    "A QUERY is irrelevant if it does not relate to these topics or falls under the following categories:\n"
-    "Politics\n"
-    "Illegal activities\n"
-    "Self-harm\n\n" 
-    "Output either TRUE or FALSE.\n"
-    "QUERY: {query}\n"
-    "Answer: "
-)
 
 # vanilla llm invoke prompt
 CHAT_PROMPT = (
@@ -53,46 +29,23 @@ CHAT_PROMPT = (
 )
 
 # history aware retriever prompt
-CONTEXTUALIZE_Q_SYSTEM_PROMPT = (
-    "Given a chat history and the latest user question"
-    "which might reference context in the chat history, formulate a standalone question"
-    "which can be understood without the chat history. Do NOT answer the question,"
-    "just reformulate it if needed and otherwise return it as is."
-)
-
-# history aware QA prompt
-HISTORY_AWARE_CHAT_PROMPT = (
-    "Context information is below.\n"
+QUERY_CONTEXTUALIZER_PROMPT = (
+    "Given a CHAT HISTORY and the latest user QUERY which might reference context in the CHAT HISTORY, " 
+    "formulate a standalone QUERY which can be understood WITHOUT the CHAT HISTORY.\n"
+    "Do NOT answer the QUERY, just reformulate it if needed and otherwise return it as is. "
+    "If the CHAT HISTOIRY is an empty list, output the QUERY as is.\n\n"
+    "The CHAT HISTORY is below.\n"
     "---------------------\n"
     "{context}\n"
     "---------------------\n"
-    "Using only the information from the context, answer the query in clear and grammatically correct sentences.\n"
-    "Note: The context may contain informal or ungrammatical phrases as it is extracted from a YouTube video.\n"
-    "Use all relevant information from the context but do not make up any facts that are not given.\n"
-    "Do not simply summarize the answer based on the given context, give a detailed and explicit answer but keep it concise.\n"
-    "Format the response properly, using paragraphs for better readability. Use bold text and bullet points where necessary, but avoid excessive markdown formatting."
+    "QUERY: {question}\n"
+    "OUTPUT: "
 )
 
 # answer for non relevant query
-IRRELEVANT_MESSAGE_PROMPT = (
-    "I'm here to answer questions related to science-based tools for everyday life, as discussed in Andrew Huberman's podcast. " 
+IRRELEVANT_QUERY_PROMPT = (
+    "Hi! I'm here to answer questions related to science-based tools for everyday life, as discussed in Andrew Huberman's podcast. " 
     "Topics include brain health, sleep, fitness, supplementation, mental health, and more. "
     "However, I couldn't find any relevant supporting documents to answer your question.\n"
     "If you have a question in these areas, feel free to ask!"
-)
-
-# relevant, irrelevant training set generation
-Q_AZER_PROMPT =(
-    """
-    I'm training an embedding model to tell whether a user query is relevant to the RAG system I'm building. 
-    My RAG system is built on Andrew Huberman's podcast, which provides science-based tools for everyday life, including topics such as Brain health, Sleep and sleep hygiene, Fitness, Supplementation, Mental health, Light and Sun exposure, Skin care, Hormones, etc. 
-    I want you to create 500 relevant questions and 500 irrelevant questions. Make sure to generate questions that a general user might ask. 
-
-    Give 2 json files, with each for relevant questions and irrelevant questions. Use the following format:
-
-    {"question": "How do I play the guitar?"}
-    {"question": "What’s the best way to make friends?"}
-    {"question": "How do I become a stand-up comedian?"}
-    ...
-    """
 )
