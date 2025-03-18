bc6f116d6f3ea6191f4af214a6b04b9a54ff406b
Seohyeong <jseoh95@gmail.com>
2025-03-18 19:44:44 +1100

    fix double instanciation issue

--- image/main.py
+++ image/main.py
@@ -9,7 +9,14 @@ from utils.utils import QueryInput, QueryOutput
 
 app = FastAPI()
 
-rag_chain = RagChatbot()
+# TODO: look into FastAPI's dependency injection
+# Get singleton instance to fix double instanciation
+def get_rag_chain():
+    global rag_chain
+    if 'rag_chain' not in globals():
+        logger.info("[FastAPI] Creating RagChatbot instance")
+        rag_chain = RagChatbot()
+    return rag_chain
 
 @app.get("/")
 def root():
@@ -18,43 +25,28 @@ def root():
 @app.post("/chat", response_model=QueryOutput)
 def chat(query_input: QueryInput):
     session_id = query_input.session_id
-    
     logger.info(f"[FastAPI] Session ID: {session_id}, User Query: {query_input.question}")
-    
     if not session_id:
         session_id = str(uuid.uuid4())
-    
     try:
-        # gets chat_history from the frontend
-        llm_output = rag_chain.invoke(query_input.question, query_input.chat_history)
+        rag_chain = get_rag_chain()
+        llm_output = rag_chain.invoke(query_input.question, query_input.chat_history) # chat_history from the frontend
         answer = llm_output.answer
         contextualized_query = llm_output.contextualized_query
         is_valid = llm_output.is_valid
-        logger.debug("[FastAPI] Processing completed successfully")
-        logger.info(f"[FastAPI] Session ID: {session_id}, AI Response: {answer}")
+        logger.info(f"[FastAPI] Session ID: {session_id}, Processing completed successfully")
     except Exception as e:
         logger.error(f"[FastAPI] Endpoint error: {str(e)}")
-    
-    return QueryOutput(session_id=session_id, answer=answer, docs=llm_output.docs,
-                       contextualized_query=contextualized_query, is_valid=is_valid)
-
+    return QueryOutput(session_id=session_id, 
+                       answer=answer, 
+                       docs=llm_output.docs,
+                       contextualized_query=contextualized_query, 
+                       is_valid=is_valid)
 
 handler = Mangum(app)
 
 if __name__ == "__main__":
     import uvicorn
-    # Run this as a server directly.
     port = 8000
     print(f"Running the FastAPI server on port {port}.")
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-    
-# TODO: initialization happens twice!
-# 2025-03-18 19:09:35 - RAG - INFO - [INIT] Initializing RAG system
-# 2025-03-18 19:09:35 - RAG - INFO - [INIT] Initializing Emebedding: text-embedding-3-small
-# 2025-03-18 19:09:35 - RAG - INFO - [INIT] Initializing LLM: gpt-4o
-# 2025-03-18 19:09:35 - RAG - INFO - [INIT] Initializing ChromaDB
-# Running the FastAPI server on port 8000.
-# 2025-03-18 19:09:37 - RAG - INFO - [INIT] Initializing RAG system
-# 2025-03-18 19:09:37 - RAG - INFO - [INIT] Initializing Emebedding: text-embedding-3-small
-# 2025-03-18 19:09:37 - RAG - INFO - [INIT] Initializing LLM: gpt-4o
-# 2025-03-18 19:09:37 - RAG - INFO - [INIT] Initializing ChromaDB
