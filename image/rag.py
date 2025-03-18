4d8f9d696296bec8997576bde7e660229d270352
Seohyeong <jseoh95@gmail.com>
2025-03-18 19:36:55 +1100

    update ragchatbot

--- image/rag.py
+++ image/rag.py
@@ -1,173 +1,140 @@
+import os
+import sys
+
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-# utils
-from utils.db_utils import init_db, init_query_db
-from utils.model_utils import init_embedding_model, init_llm_model
-from utils.prompt_utils import docs2str, CHAT_PROMPT, QUERY_CONTEXTUALIZER_PROMPT, IRRELEVANT_QUERY_PROMPT
-from utils.utils import init_config, RAGDoc, RAGOutput
+from utils.prompt_utils import docs2str, \
+    CHAT_PROMPT, QUERY_CONTEXTUALIZER_PROMPT, IRRELEVANT_QUERY_PROMPT, ERROR_ANSWER_PROMPT
+from utils.utils import RAGOutput, convert_to_ragdoc
 from config.logger_config import logger
+from config.model_config import get_config
 
+IS_USING_IMAGE_RUNTIME = bool(os.environ.get("IS_USING_IMAGE_RUNTIME", False))
+if IS_USING_IMAGE_RUNTIME:
+    __import__("pysqlite3")
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+    
+import chromadb
+import chromadb.utils.embedding_functions as embedding_functions
 
+    
 class RagChatbot():
-    def __init__(self, llm_model_name):
-        logger.info("Initializing RAG system")
-        self.llm_model_name = llm_model_name
+    def __init__(self):
+        # get config
+        logger.info("[INIT] Initializing RAG system")
         try:
-            self.config = init_config(self.llm_model_name)
-            logger.debug("Configuration initialized")
+            self.config = get_config()
         except Exception as e:
-            logger.error(f"Failed to initialize config: {str(e)}")
+            logger.error(f"[INIT] Failed to initialize config: {str(e)}")
             
-        
         # embedding model
-        logger.info("Initializing emebedding")
+        logger.info(f"[INIT] Initializing Emebedding: {self.config.embedding_model}")
         try:
-            self.embedding_function = init_embedding_model(self.config.embedding_model)
-            logger.debug("Embedding model initialized")
-        except Exception as e:
-            logger.error(f"Failed to initialize embedding model: {str(e)}")
-        
-        # query retriever
-        logger.info("Initializing query vectorstore and retriever")
-        try:
-            self.query_vectorstore = init_query_db(self.config, self.embedding_function)
-            self.query_retriever = self.query_vectorstore.as_retriever(
-                search_type=self.config.qr_search_type, 
-                search_kwargs={"k": self.config.qr_top_k}
+            self.embedding_function = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=os.getenv("OPENAI_API_KEY"),
+                model_name=self.config.embedding_model
                 )
-            logger.debug("Query vectorstore and retriever initialized")
         except Exception as e:
-            logger.error(f"Failed to initialize query vectorstore and retriever: {str(e)}")
-        
-        # basic retriever
-        logger.info("Initializing RAG vectorstore and retriever")
-        try:
-            self.vectorstore = init_db(self.config, self.embedding_function)
-            self.retriever = self.vectorstore.as_retriever(
-                search_type=self.config.rr_search_type, 
-                search_kwargs={"score_threshold": self.config.rr_score_threshold}
-                )
-            logger.debug("RAG vectorstore and retriever initialized")
-        except Exception as e:
-            logger.error(f"Failed to initialize RAG vectorstore and retriever: {str(e)}")
+            logger.error(f"[INIT] Failed to initialize embedding model: {str(e)}")
             
         # llm
-        logger.info("Initializing LLM")
+        logger.info(f"[INIT] Initializing LLM: {self.config.generation_model}")
         try:
-            self.llm = init_llm_model(self.config.generation_model)
-            logger.debug("LLM initialized")
+            self.llm = ChatOpenAI(model=self.config.generation_model,
+                                  api_key=os.getenv("OPENAI_API_KEY"))
         except Exception as e:
-            logger.error(f"Failed to initizlize LLM: {str(e)}")
-            
-    def retrieve_with_score(self, query: str, k: int) -> RAGOutput:
-        """basic retriever"""
-        docs, scores = zip(*self.vectorstore.similarity_search_with_relevance_scores(query, k=k))
-        return RAGOutput(docs=[RAGDoc(video_id=doc.metadata.get("video_id"),
-                                        title=doc.metadata.get("video_title"),
-                                        header=doc.metadata.get("video_header"),
-                                        time_start=doc.metadata.get("time_start"),
-                                        time_end=doc.metadata.get("time_end"),
-                                        segment_idx=doc.metadata.get("segment_idx"),
-                                        score=score)
-                                for doc, score in zip(docs, scores)])
+            logger.error(f"[INIT] Failed to initizlize LLM: {str(e)}")
         
+        # chroma client (TODO: add host and port to config)
+        logger.info("[INIT] Initializing ChromaDB")
+        try:
+            chroma_client = chromadb.HttpClient(self.config.chroma_host, int(self.config.chroma_port))
+            self.query_collection = chroma_client.get_collection(name=self.config.query_collection_name)
+            self.doc_collection = chroma_client.get_collection(name=self.config.doc_collection_name)
+        except Exception as e:
+            logger.error(f"[INIT] Failed to initialize ChromaDB: {str(e)}")
+
     def validate_query(self, query: str) -> bool:
-        docs = self.query_retriever.invoke(query)
-        num_relevant = sum(1 for doc in docs if doc.metadata["is_relevant"])
-        if num_relevant >= self.config.qr_top_k // 2:
-            return True
-        else:
-            return False
-        
-    def filter_docs(self, docs):
-        filtered_docs = []
-        video_ids = set()
-        for doc in docs:
-            video_id = "_".join(doc.id.split("_")[:-1])
-            if video_id not in video_ids:
-                filtered_docs.append(doc)
-                video_ids.add(video_id)
-        return filtered_docs
+        docs = self.query_collection.query(
+            query_embeddings=self.embedding_function([query]),
+            query_texts=[query],
+            n_results=self.config.query_top_k
+        )
+        num_relevant = sum(1 for x in docs["metadatas"][0] if x["is_relevant"])
+        result = True if num_relevant >= self.config.query_top_k // 2 else False
+        return result
     
-    def get_contextualized_query(self, query, chat_history):
+    def get_contextualized_query(self, query: str, chat_history: list) -> str:
         prompt = ChatPromptTemplate.from_template(QUERY_CONTEXTUALIZER_PROMPT)
         response = self.llm.invoke(prompt.format_messages(context=chat_history, question=query))
         return response.content
         
     def invoke(self, query: str, chat_history: list) -> RAGOutput:
-        logger.info("Contextualizing the query")
+        logger.info("********** Invoking RagChatbot **********")
+        logger.info(f"Query: '{query}'")
+        
+        # query contextualization
         try:
             contextualized_query = self.get_contextualized_query(query, chat_history)
-            logger.debug("Query is contextualized")
         except Exception as e:
+            contextualized_query = None
             logger.error(f"Failed to contextualized the query: {str(e)}")
+        logger.info(f"Contextualizing query: '{contextualized_query}'")
             
-        logger.info("Retrieving relevant documents")
-        try:
-            docs = self.retriever.invoke(contextualized_query)
-            docs = docs[:3] # only pass top 3 filtered by score
-            logger.debug("Relevant docs retrieved")
-        except Exception as e:
-            logger.error(f"Failed to retrieve documents: {str(e)}")
-            
+        # query validation
         logger.info("Validating query")
         try:
             is_valid = self.validate_query(contextualized_query)
-            logger.debug("Query is validated")
         except Exception as e:
+            is_valid = None
             logger.error(f"Failed to validate query: {str(e)}")
-            
-        if is_valid and docs:
+        
+        # TODO: change in logic (docs are not considered)
+        if is_valid and (is_valid is not None):
+            # rag retrieval
+            logger.info("Retrieving relevant documents")
+            try:
+                docs = self.doc_collection.query(
+                    query_embeddings=self.embedding_function([contextualized_query]),
+                    query_texts=[contextualized_query],
+                    n_results=self.config.doc_top_k
+                )
+                docs = convert_to_ragdoc(docs)
+            except Exception as e:
+                docs = []
+                logger.error(f"Failed to retrieve documents: {str(e)}")
+        
+            # run generation
             logger.info("Generating the response")
             try:
                 context = docs2str(docs)
                 prompt = ChatPromptTemplate.from_template(CHAT_PROMPT)
                 llm_response = self.llm.invoke(prompt.format(context=context, question=contextualized_query))
-                logger.debug("Response generated")
+                answer = llm_response.content
             except Exception as e:
-                logger.error("Failed to generate the response")
-                
-            filtered_docs = self.filter_docs(docs)     
-            
-            return RAGOutput(answer=llm_response.content,
-                            docs=[RAGDoc(video_id=doc.metadata.get("video_id"),
-                                            title=doc.metadata.get("video_title"),
-                                            header=doc.metadata.get("video_header"),
-                                            time_start=doc.metadata.get("time_start"),
-                                            time_end=doc.metadata.get("time_end"),
-                                            segment_idx=doc.metadata.get("segment_idx"))
-                                    for doc in filtered_docs],
-                            contextualized_query=contextualized_query,
-                            is_valid=True)
+                answer = ERROR_ANSWER_PROMPT
+                logger.error("Failed to generate the response")    
         else:
-            return RAGOutput(answer=IRRELEVANT_QUERY_PROMPT, 
-                             contextualized_query=contextualized_query,
-                             is_valid=False)
-    
-
-def evaluate():
-    rag_chain = RagChatbot("openai")
-    from utils.eval_utils import test_retriever
-    test_retriever(rag_chain, k=3, threshold=0.5)
+            answer = IRRELEVANT_QUERY_PROMPT
+            docs = []
+        return RAGOutput(answer=answer,
+                         docs=docs,
+                         contextualized_query=contextualized_query,
+                         is_valid=is_valid)
     
     
 def main():
-    rag_chain = RagChatbot("openai")
+    rag_chain = RagChatbot()
     
     questions = [
         "Hello",
-        "Is chemical sunscreen bad for you?",
-        "Where can I find it?",
-        "How do I kill myself?",
-        "What is a cosine function?"
+        "Tell me about nicotine.",
+        "How does it affect sleeping?",
+        "How do I kill someone?",
+        "How do I fix a computer?"
         ]
-    
-    if rag_chain.config.activate_mlflow:
-        import mlflow
-        from utils.utils import start_mlflow_server, stop_mlflow_server
-        mlflow_process = start_mlflow_server(rag_chain.config.host, rag_chain.config.port)
-        mlflow.set_tracking_uri(f"http://{rag_chain.config.host}:{rag_chain.config.port}")
      
     chat_history = []
     
@@ -189,8 +156,9 @@ def main():
             else:
                 f.write("----------------------------------------\n")
                 f.write(f"INVALID QUERY\n")   
-                
-            if llm_output.docs:  
+            
+            # TODO: attach to history when both is_valid and docs found
+            if llm_output.is_valid and llm_output.docs:  
                 f.write("----------------------------------------\n")
                 f.write(f"DOCS FOUND\n")
                 chat_history.extend([
@@ -204,10 +172,6 @@ def main():
             f.write("----------------------------------------\n")
             f.write(f"ANSWER: {llm_output.answer}\n")          
 
-                    
-    if rag_chain.config.activate_mlflow:
-        stop_mlflow_server(mlflow_process)
 
 if __name__ == "__main__":
     main()
-    # evaluate()
